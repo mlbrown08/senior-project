b902224ee70171edadd4725a0fa56e93fae78096
mlbrown08 <mlbrown7@uncg.edu>
2014-10-14 23:17:29 -0400

    Integrated User, Projects, and Teams Modules

--- controllers/team.js
+++ controllers/team.js
@@ -40,7 +40,7 @@ module.exports.controller = function (app) {
    */
 
   app.get('/teamlist', passportConf.isAuthenticated, passportConf.isAdministrator, function (req, res) {
-    Team.find({}).populate('users').exec( function (err, items) {
+    Team.find({}).populate('users projects').exec( function (err, items) {
       if (err) {
         return (err, null);
       }
@@ -54,7 +54,7 @@ module.exports.controller = function (app) {
    */
 
   app.get('/teamlist/:id', passportConf.isAuthenticated, passportConf.isAdministrator, function (req, res) {
-    Team.findOne({ _id : req.params.id }).populate('users').exec( function (err, item) {
+    Team.findById(req.params.id).populate('users projects').exec( function (err, item) {
       if (err) {
         return (err, null);
       }
@@ -68,19 +68,27 @@ module.exports.controller = function (app) {
    */
 
   app.delete('/teamlist/:id', passportConf.isAuthenticated, passportConf.isAdministrator, function (req, res) {
-    // Loops through Users and removes them from the team
-    Team.findOne({ _id : req.params.id }, function (err, team) {
+    Team.findById(req.params.id, function (err, team) {
+      // Loops through Users and removes them from the team
       team.users.forEach( function (user) {
-        User.findByIdAndUpdate({ _id:user }, { $pull: { teams:team._id }}, function (err) {
+        User.findByIdAndUpdate(user, { $pull: { teams:team._id }, $pullAll: { projects:team.projects }}, function (err) {
           if (err) {
-            console.log(err);
-            res.send({ msg: 'User error: ' + err });
+            return res.send({ msg: err });
           }
         });
       });
+
+      // Loops through the Projects and removes them from the team
+      team.projects.forEach( function (project) {
+        Project.findByIdAndUpdate(project, { $pull: { teams:team._id }}, function (err) {
+          if (err) {
+            return res.send({ msg: err });
+          }
+        });
+      });
+
       // Delete Team
       Team.remove({ _id : req.params.id }, function (err, result) {
-        console.log(result);
         res.send((result === 1) ? { msg: '' } : { msg: 'error: ' + err });
       });
     });
@@ -92,12 +100,12 @@ module.exports.controller = function (app) {
    */
 
   app.delete('/teamdeleteuser', passportConf.isAuthenticated, passportConf.isAdministrator, function (req, res) {
-    // Removes User from the team
-    User.findByIdAndUpdate(req.body.user_id, { $pull: { teams:req.body.team_id }}, function (err, result) {
+    // Removes User from the Team
+    Team.findByIdAndUpdate(req.body.team_id, { $pull: { users:req.body.user_id }}, function (err, team) {
       if (err) {
         res.send({ msg: err });
       }
-      Team.findByIdAndUpdate(req.body.team_id, { $pull: { users:req.body.user_id }}, function (err, result) {
+      User.findByIdAndUpdate(req.body.user_id, { $pull: { teams:req.body.team_id }, $pullAll: { projects: team.projects }}, function (err, result) {
         res.send((err === null) ? { msg: '' } : { msg: 'Team error: ' + err });
       });
     });
@@ -109,14 +117,78 @@ module.exports.controller = function (app) {
    */
 
   app.post('/teamaddusers', passportConf.isAuthenticated, passportConf.isAdministrator, function (req, res) {
-    User.update({ _id: { $in: req.body.users }}, { $push: { teams:req.body.team_id }}, { multi: true }, function (err, result) {
+    // Adds Users to the Team
+    Team.findByIdAndUpdate(req.body.team_id, { $pushAll: { users:req.body.users }}, function (err, team) {
       if (err) {
-        res.send( { msg: 'error: ' + err });
+        return res.send( { msg: 'error: ' + err });
       }
-      Team.findByIdAndUpdate(req.body.team_id, { $pushAll: { users:req.body.users }}, function (err, result) {
+      User.update({ _id: { $in: req.body.users }}, { $push: { teams:req.body.team_id }, $pushAll: { projects:team.projects }}, { multi: true }, function (err, result) {
         res.send((err === null) ? { msg: '' } : { msg: 'error: ' + err });
       });
     });
+  });
+
+  /**
+   * DEL /teamdeleteproject
+   * JSON delete project from team api
+   */
+
+  app.delete('/teamdeleteproject', passportConf.isAuthenticated, passportConf.isAdministrator, function (req, res) {
+    // Removes Project from the Team
+    Project.findByIdAndUpdate(req.body.project_id, { $pull: { teams:req.body.team_id }}, function (err, result) {
+      if (err) {
+        return res.send({ msg: err });
+      }
+      Team.findByIdAndUpdate(req.body.team_id, { $pull: { projects:req.body.project_id }}, function (err, result) {
+        if (err) {
+          return res.send({ msg: err });
+        }
+      });
+      // Loops through the Users of the Team and removes the Project from them
+      Team.findById(req.body.team_id, function (err, team) {
+        var users = team.users;
+        users.forEach( function (user) {
+          User.findByIdAndUpdate(user, { $pull: { projects:req.body.project_id }}, function (err) {
+            if (err) {
+              return res.send({ msg: err });
+            }
+          });
+        });
+      });
+    });
+    // Everything delete and update correctly so return success
+    return res.send({ msg: '' });
+  });
+  
+  /**
+   * POST /teamaddprojects
+   * JSON add projects to team api
+   */
+
+  app.post('/teamaddprojects', passportConf.isAuthenticated, passportConf.isAdministrator, function (req, res) {
+    Project.update({ _id: { $in: req.body.projects }}, { $push: { teams:req.body.team_id }}, { multi: true }, function (err, result) {
+      if (err) {
+        return res.send( { msg: 'error: ' + err });
+      }
+      Team.findByIdAndUpdate(req.body.team_id, { $pushAll: { projects:req.body.projects }}, function (err, result) {
+        if (err) {
+          return res.send( { msg: 'error: ' + err });
+        }
+      });
+      // Loops through the Users of the Team and add the Project to them
+      Team.findById(req.body.team_id , function (err, team) {
+        var users = team.users;
+        users.forEach( function (user) {
+          User.update({ _id:user }, { $pushAll: { projects:req.body.projects }}, function (err) {
+            if (err) {
+              return res.send( { msg: 'error: ' + err });
+            }
+          });
+        });
+      });
+    });
+    // Everything Add and update correctly so return success
+    return res.send({ msg: '' });
   });
 
   /**
@@ -165,30 +237,54 @@ module.exports.controller = function (app) {
      */
 
     workflow.on('createTeam', function () {
-      // Get number of keys on parameters
-      var size = Object.keys(req.body).length;
+      // Variables to hold users and projects count
+      var userCount = 0;
+      var projectCount = 0;
+
+      // Note - If the is only 1 user or project the value with be 24
+      // because it the treats the value as an array with 24 characters
+      for (var key in req.body ) {
+        if (key === 'user') {
+          userCount = req.body.user.length;
+        }
+        else if (key === 'project') {
+          projectCount = req.body.project.length;
+        }
+      }
 
       // Create an array to hold the users
       // Have to process the users differently 
       // if more then 1 is checked
       var usersArray = [];
-      if (size < 3) {
-        for (var key in req.body ) {
-          if (key === 'user') {
-            usersArray.push(req.body[key]);
-          }
-        }
+      if (userCount === 0) {
+        usersArray.push(null);
+      }
+      else if (userCount === 24) { // Remember this is really one user
+        usersArray.push(req.body.user);
       }
       else {
-        for (var user in req.body.user ) {
-          usersArray.push(req.body.user[user]);
-        }
+        usersArray = req.body.user;
       }
-
+      
+      // Create an array to hold the projects
+      // Have to process the users differently 
+      // if more then 1 is checked
+      var projectsArray = [];
+      if (userCount === 0) {
+        usersArray.push(null);
+      }
+      else if (projectCount === 24) { // Remeber this is really on user
+        projectsArray.push(req.body.project);
+      }
+      else {
+        projectsArray = req.body.project;
+      }
+      
       // Create team
       var team = new Team({
-        name:           req.body.name.trim(),
-        users:          usersArray
+        name: req.body.name.trim(),
+        users: usersArray,
+        projects: projectsArray
       });
       
       // Save team
@@ -199,19 +295,29 @@ module.exports.controller = function (app) {
           }
           req.flash('error', { msg: 'Could not create team!' });
           return res.redirect('back');
-        } else {
-          // next step, update users
-          workflow.emit('updateUsers', usersArray);
         }
       });
+
+      // next step, update users and projects
+      if (userCount > 0) {
+        workflow.emit('updateUsers', usersArray, projectsArray);
+      }
+      if (projectCount > 0) {
+        workflow.emit('updateProjects', projectsArray);
+      }
+
+      req.flash('info', { msg: 'Your team has been successfully created.' });
+      return res.redirect('/teams');
+      
+      // WORKFLOW COMPLETED
     });
 
     /**
      * Step 3: Add users to the team
      */
 
-    workflow.on('updateUsers', function (users) {
-      // Loops through Users selected and updates their team field
+    workflow.on('updateUsers', function (users, projects) {
+      // Loops through Users selected and add them to the Team
       users.forEach( function (user) {
         User.findById(user, function (err, user) {
           if (err) {
@@ -220,7 +326,7 @@ module.exports.controller = function (app) {
           } 
 
           Team.findOne({ name: req.body.name }, function (err, team) {
-            User.update({ _id:user._id }, { $push: { teams:team._id }}, function (err) {
+            user.update( { $push: { teams:team._id }, $pushAll: { projects:projects }}, function (err) {
               if (err) {
                 req.flash('error', { msg: 'Could not add User to Team!' });
                 return res.redirect('back');
@@ -229,12 +335,31 @@ module.exports.controller = function (app) {
           });
         });
       });
-      
-      req.flash('info', { msg: 'Your team has been successfully created.' });
-      return res.redirect('/teams');
-      
-      // WORKFLOW COMPLETED
+    });
 
+    /**
+     * Step 4: Add projects to the team
+     */
+
+    workflow.on('updateProjects', function (projects) {
+      // Loops through Projects selected and adds them to the Team
+      projects.forEach( function (project) {
+        Project.findById(project, function (err, project) {
+          if (err) {
+            req.flash('error', { msg: 'Could not find Project!' });
+            return res.redirect('back');
+          } 
+
+          Team.findOne({ name: req.body.name }, function (err, team) {
+            project.update({ $push: { teams:team._id }}, function (err) {
+              if (err) {
+                req.flash('error', { msg: 'Could not add Project to Team!' });
+                return res.redirect('back');
+              }
+            });
+          });
+        });
+      });
     });
 
     /**
@@ -306,7 +431,7 @@ module.exports.controller = function (app) {
         });
       
         req.flash('info', { msg: 'Your team has been successfully updated.' });
-        return res.redirect('back');
+        return res.redirect('/teams');
       });
       
       // WORKFLOW COMPLETED
